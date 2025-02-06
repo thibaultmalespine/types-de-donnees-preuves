acb4b681ef6755665ca8900c6317d447218cdbda
fromScratch-ToDev <145133454+fromScratch-ToDev@users.noreply.github.com>
2025-02-06 21:46:41 +0100

    fin de l'erxercice 7

--- type_preuve.ml
+++ type_preuve.ml
@@ -190,3 +190,33 @@ type expr =
   | Var of string
   | Abs of string * tp * expr
   | App of expr * expr;;
+
+
+exception Type_error of string;;
+
+let rec tp_of_expr ctx expr =
+  match expr with
+  | Const (BoolE _) -> ConstT BoolT
+  | Const (IntE _) -> ConstT IntT
+  | Var x -> (try List.assoc x ctx with Not_found -> raise (Type_error ("Unbound variable: " ^ x)))
+  | Abs (x, t, e) -> FunT (t, tp_of_expr ((x, t) :: ctx) e)
+  | App (e1, e2) ->
+      let t1 = tp_of_expr ctx e1 in
+      let t2 = tp_of_expr ctx e2 in
+      (match t1 with
+        | FunT (arg_t, ret_t) ->
+            if arg_t = t2 then ret_t
+            else raise (Type_error (string_of_expr e1 ^" !! " ^ string_of_expr e2 ^ " !!" ^
+                                  "This expression has type " ^ string_of_type t2 ^ 
+                                  " but an expression was expected of type " ^ string_of_tp arg_t))
+        | _ -> raise (Type_error "Application of a non-function expression"));;
+
+
+(* TEST *)
+tp_of_expr [] (App (Abs ("x", ConstT IntT, Var "x"), Const (IntE 2))) ;;
+tp_of_expr [] (App (Abs ("x", ConstT IntT, Var "x"), Const (BoolE true)));;
+
+tp_of_expr [] (App (Abs ("x", ConstT IntT, Var "x"), Const (IntE 2)));;
+tp_of_expr [] (App ( App ( Abs("x", ConstT IntT, Var "x"), Const (IntE 2)), Const (IntE 3)));;
+tp_of_expr [] (App (App ( Abs ("x", ConstT IntT, Abs ("y", ConstT BoolT, Var "y")) , Const (IntE 2)), Const (IntE 3)));;
+tp_of_expr [] (App (Abs("f", FunT(ConstT IntT, ConstT BoolT), App( Var "f", Const (IntE 2))) , Abs ("x", ConstT IntT, Const (BoolE true))));;
